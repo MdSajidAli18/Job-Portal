bc885c375cf65dec77118d7683d871d6439e1e7b
Sajid <mdsajidali1803@gmail.com>
2024-09-01 00:57:48 +0530

    updated job model

--- backend/models/job.model.js
+++ backend/models/job.model.js
@@ -17,6 +17,10 @@ const jobSchema = new mongoose.Schema({
         type: Number,
         required: true
     },
+    experience: {
+        type: Number,
+        required: true
+    },
     location: {
         type: String,
         required: true
