1fc584d309ce95b5ccd4ff83a16d47d6e93ee4a2
Sajid <mdsajidali1803@gmail.com>
2024-09-01 03:57:21 +0530

    Job route

---
+++ backend/routes/job.route.js
@@ -0,0 +1,14 @@
+import express from "express";
+import isAuthenticated from "../middlewares/isAuthenticated.js";
+import { getAllJobs, getJobById, getJobPostedByRecruiter, postJob } from "../controllers/job.controller.js";
+
+
+const router = express.Router();
+
+router.route("/post").post(isAuthenticated, postJob);
+router.route("/get").get(isAuthenticated, getAllJobs);
+router.route("/get/:id").get(isAuthenticated, getJobById);
+router.route("/getRecruiterJobs").get(isAuthenticated, getJobPostedByRecruiter);
+
+
+export default router;
