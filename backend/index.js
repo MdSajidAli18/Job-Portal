2de6ef62bff58a4873128ef8ad73265fef0e6170
Sajid <mdsajidali1803@gmail.com>
2024-09-07 22:16:09 +0530

    updated files

--- backend/index.js
+++ backend/index.js
@@ -19,8 +19,8 @@ app.use(express.urlencoded({extended: true}));
 app.use(cookieParser());
 
 const corsOptions = {
-    origin: 'http//localhost:5173',
-    Credentials: true
+    origin: 'http://localhost:5173',
+    credentials: true
 }
 app.use(cors(corsOptions));
 
